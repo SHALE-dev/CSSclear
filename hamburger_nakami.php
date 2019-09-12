a54b70bdb9ccecd29828f1ea151bcdcd70e92978
koevi97118 <46910102+koevi97118@users.noreply.github.com>
2019-09-12 12:18:52 -0400

    Create hamburger_nakami.php

---
+++ hamburger_nakami.php
@@ -0,0 +1,38 @@
+<div id="hambuger-box">
+   <div class="hambuger-item1">
+      <div class="hamburger-items">
+<img src="https://mute-river-53450524.stg-s.snapup.jp/wp/wp-content/uploads/2019/09/人気-1-1-1-1-1-2-1.jpg">
+         <p><span>HowToSHALE</span><br>新しいおしゃれの教科書</p></div>
+   </div>
+   <div class="hambuger-item2">
+      <div class="hamburger-items">
+<img src="https://mute-river-53450524.stg-s.snapup.jp/wp/wp-content/uploads/2019/09/教科書-1-1.jpg">
+         <p><span>TOP PAGE</span><br>トップページへ</p></div>
+   </div>
+   <div class="hambuger-item3">
+      <div class="hamburger-items">
+<img src="https://mute-river-53450524.stg-s.snapup.jp/wp/wp-content/uploads/2019/09/人気-1-1-1-1-1-2-1.jpg">
+         <p><span>POPULAR</span><br>人気記事一覧へ</p></div>
+   </div>
+   <div class="hambuger-item4">
+      <div class="hamburger-items">
+<img src="https://mute-river-53450524.stg-s.snapup.jp/wp/wp-content/uploads/2019/09/教科書-1-1.jpg">
+         <p><span>GENRE</span><br>ファッションの系統から記事を探す</p></div>
+   </div>
+   <div class="hambuger-item5">
+      <div class="hamburger-items">
+<img src="https://mute-river-53450524.stg-s.snapup.jp/wp/wp-content/uploads/2019/09/人気-1-1-1-1-1-2-1.jpg">
+         <p><span>ITEMS</span><br>自分が持っているアイテムからコーデ記事を探す</p></div>
+   </div>
+   <div class="hambuger-item6">
+      <div class="hamburger-items">
+<img src="https://mute-river-53450524.stg-s.snapup.jp/wp/wp-content/uploads/2019/09/人気-1-1-1-1-1-2-1.jpg">
+         <p><span>INSTAGRAM</span><br>SHALE公式Instagramはこちら</p></div>
+   </div>
+
+</div>
+
+<div class="hambuger-shale-rotate">
+<div class="hambuger-menu-shale">
+<p>SHALE</p>
+</div></div>
